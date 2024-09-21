fcdc2b52815266abf77f0985efad5564ed7180d0
almajacob <almajacob7@gmail.com>
2024-09-21 18:12:53 +0530

    done

--- flowerms.py
+++ flowerms.py
@@ -1,6 +1,6 @@
 #flowershop management system
 import datetime
-print(datetime.datetime.now().strftime("%x"))
+
 flowers=[]
 while True:
     print('''
@@ -9,71 +9,94 @@ while True:
 3. Remove Flower
 4. View All Flowers
 5. Search Flower
-6. Exit
+6.view available flowers
+7. Exit
     ''')
     choice=int(input("enter ur choice"))
     if choice==1:
-          name = input("Name of Flower: ")
-          qty = int(input("Quantity of Flower: "))
+          flower_name = input("Name of Flower: ")
+          flower_qty = (input("Quantity of Flower: "))
           price = (input("price of Flower: "))
           pay = input("Modes of Payment: ")
-          date = datetime.datetime.now().strftime("%x")
-          flowers.append({'name': name, 'qty': qty, 'price':price, 'pay': pay, 'date': date})
+          flower_sts=input('available or unavailable ')
+        
+          flowers.append({'flower_name': flower_name, 'flower_qty': flower_qty, 'price':price, 'pay': pay,'flower_sts': flower_sts})
     elif choice==2:
-        name = input("Name of Flower: ")
+        flower_name = input("Name of Flower: ") 
         f=0
         for i in flowers:
-                  if i['name']==name:
+                  if i['flower_name']==flower_name:
                       f=1
                       while True:
                         print('''
 1.  update Quantity
 2. update price
 3.update Mode of Payment
-4. Exit
+
+4.updated flower_sts 
+5. Exit
 '''
 )  
                         sub_choice = int(input("Enter Your updated choice: "))
                         if sub_choice == 1:
-                                   new_qty = int(input("Enter New Quantity: "))
-                                   flowers['qty'] = new_qty
+                            new_qty = int(input("Enter New Quantity: "))
+                            i['flower_qty'] =new_qty
                         elif sub_choice == 2:
                              new_price = (input("Enter New price: "))
-                             flowers['price'] = new_price
+                             i['price'] =new_price
                         elif sub_choice== 3:
                              new_pay = input(" enter updated Modes of Payment: ")
-                             flowers['pay'] = new_pay
+                             i['pay'] =new_pay
                         elif sub_choice == 4:
+                             new_sts = input(" enter updated sts of flower(available or unavailable): ")
+                             i['flower_sts'] =new_sts
+                        elif sub_choice==5:
+                                 
                             break
+                        else:
+                            print("invalid choice")
         if f==0:
-            print('Flower Name Not Found')
+            print(' Not Found')
     elif choice==3:
-         name=input("Enter Name Of The Product :")
+         flower_name=input("Enter Name Of The flower :")
          f==0
          for i in flowers:
-              if i['name']==name:
-                   flowers.remove(i)
-                   f=1
-              if f==0:
-                      print("Product Name Not Found")
+              if i['flower_name']==flower_name:
+                    f=1
+                    flowers.remove(i)
+                  
+         if f==0:
+               print(" Not Found")
     elif choice==4:
-                print('{:<10} {:<5}{:<10}'.format('name','qty','price','date'))  
+                print('_'*30)
+                print('{:<15} {:<10}  {:<20} {:<15}'.format('flower_name','flower_qty','price','flower_sts'))  
                 print('_'*30)
                 for i in flowers:
-                     print('{:<10} {:<5}{:<10}'.format(i['name'],i['qty'],i['price'],i['date']))
-    elif choice==5:
-         name=input("Name Of flower :")
-         f=0
+                     print('{:<15} {:<10}  {:<20} {:<15}'.format(i['flower_name'],i['flower_qty'],i['price'],i['flower_sts']))
+    elif choice==5: 
+         flower_search=input(" enter Name Of flower you want :")
+         print('_'*30)
+         print('{:<15} {:<10}  {:<20}'.format('flower_name','flower_qty','price','flower_sts'))  
+         print('_'*30)
          for i in flowers:
-              if i['name']==name:
-                    print(i)
-                    f=1
-              if f==0:
-                     print("flower Name Not Found")
+               if i['flower_name']==flower_search:
+              
+                     print('{:<15} {:<10}  {:<20} {:<15}'.format(i['flower_name'],i['flower_qty'],i['price'],i['flower_sts']))
+                     
+               else:      
+                     print(" Not Found")
     elif choice ==6:
-        break
+          print('_'*30)
+          print('{:<15} {:<10}  {:<20} {:<15}'.format('flower_name','flower_qty','price','flower_sts'))  
+          print('_'*30)
+          for i in flowers:
+                if i['flower_sts']=='available':
+                     print('{:<15} {:<10}  {:<20} {:<15}'.format(i['flower_name'],i['flower_qty'],i['price'],i['flower_sts']))
+    elif choice==7:
+                     break
     else:
-        print("invalid choice")                                        
+                     print("invalid option")     
+                                              
                             
                          
                                          
